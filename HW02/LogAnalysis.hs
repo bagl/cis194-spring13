3507606bf6dbfa9f4d6933e47bd7cd7c650bb405
Petr Vápenka <petr.vapenka@synthosgroup.com>
2015-07-01 10:39:12 +0200

    Avoid using partial function `read`

--- HW02/LogAnalysis.hs
+++ HW02/LogAnalysis.hs
@@ -5,15 +5,19 @@ module HW02.LogAnalysis where
 
 import StrippedPrelude
 import HW02.Log
-import Text.Read
+
+import Control.Applicative ((<$>), (<*>), pure)
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
 
 parseMessage :: String -> LogMessage
-parseMessage line = pm $ words line
+parseMessage line = fromMaybe (Unknown line) (pm $ words line)
   where
-    pm ("I":ts:msg)    = LogMessage Info              (read ts) $ unwords msg
-    pm ("W":ts:msg)    = LogMessage Warning           (read ts) $ unwords msg
-    pm ("E":le:ts:msg) = LogMessage (Error $ read le) (read ts) $ unwords msg
-    pm _               = Unknown line
+    pm :: [String] -> Maybe LogMessage
+    pm ("I":ts:msg)    = LogMessage Info                          <$> readMaybe ts <*> pure (unwords msg)
+    pm ("W":ts:msg)    = LogMessage Warning                       <$> readMaybe ts <*> pure (unwords msg)
+    pm ("E":le:ts:msg) = LogMessage <$> fmap Error (readMaybe le) <*> readMaybe ts <*> pure (unwords msg)
+    pm _               = Nothing
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
@@ -21,9 +25,9 @@ parse = map parseMessage . lines
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _)            t            = t
 insert lm                     Leaf         = Node Leaf lm Leaf
-insert lm@(LogMessage _ ts _) (Node l m r) = 
+insert lm@(LogMessage _ ts _) (Node l m r) =
   case m of
-    Unknown _             -> Node l m r
+    Unknown _             -> Node l m r -- should not occur if MessageTree is build with insert
     LogMessage _ nodeTS _ ->
       if ts < nodeTS
         then Node (insert lm l) m r
@@ -37,7 +41,7 @@ inOrder Leaf = []
 inOrder (Node l m r) = inOrder l ++ [m] ++ inOrder r
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = map toMsg . filter isErr50 . inOrder . build
+whatWentWrong = map toMsg . inOrder . build . filter isErr50
   where
     isErr50 (LogMessage (Error l) _ _) = l >= 50
     isErr50 _                          = False
