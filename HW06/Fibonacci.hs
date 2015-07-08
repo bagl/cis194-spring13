86f7defab329731cad4c515aa80f770b50c7c87d
Petr Vapenka <petr.vapenka@volny.cz>
2015-07-08 10:24:37 -0700

    Implement Stream and basic method for working with them.

--- HW06/Fibonacci.hs
+++ HW06/Fibonacci.hs
@@ -12,3 +12,21 @@ fibs1 = map fib [0..]
 
 fibs2 :: [Integer]
 fibs2 = 0 : 1 : zipWith (+) fibs2 (drop 1 fibs2)
+
+data Stream a = Cons a (Stream a)
+
+instance Show a => Show (Stream a) where
+  show = ("Stream "++) . show . take 20 . streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Cons a s) = a : streamToList s
+
+streamRepeat :: a -> Stream a
+streamRepeat x = let s = Cons x s
+                 in s
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons a s) = Cons (f a) $ streamMap f s
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f a = Cons a $ streamFromSeed f (f a)
