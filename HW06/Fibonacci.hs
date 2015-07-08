50b3d16d9eb630970eade2993ddae8ad1290fc4a
Petr Vapenka <petr.vapenka@volny.cz>
2015-07-08 10:06:51 -0700

    HW06 - impement fibs2

--- HW06/Fibonacci.hs
+++ HW06/Fibonacci.hs
@@ -3,10 +3,12 @@ module HW06.Fibonacci where
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
-fib n = fib (n - 1) + fib (n - 2)
+fib n
+  | n > 1     = fib (n - 1) + fib (n - 2)
+  | otherwise = error "Cannot call fib with negative number"
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
 
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = 0 : 1 : zipWith (+) fibs2 (drop 1 fibs2)
