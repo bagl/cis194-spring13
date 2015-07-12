32419710169a10aaf28902f98cf694c32196a926
Petr Vapenka <petr.vapenka@volny.cz>
2015-07-12 11:52:39 -0700

    Delete unused stuff

--- HW07/JoinListBufEditor.hs
+++ HW07/JoinListBufEditor.hs
@@ -1,10 +1,7 @@
 module HW07.Main where
 
-import Data.Monoid (mempty)
-
 import HW07.JoinList
 import HW07.Editor
 
--- TODO: Slow!!!!!!!!!!!!!!!!!
 main :: IO ()
 main = runEditor editor $ scoreSizeLine "Testovaci string"
