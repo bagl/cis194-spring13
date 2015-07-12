b626b3701342f2ae7a1f26af9014e01d97d64e72
Petr Vapenka <petr.vapenka@volny.cz>
2015-07-12 03:36:30 -0700

    Add more tests

--- Test.hs
+++ Test.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Main where
 
 import Control.Applicative ((<$>), (<*>))
 import Test.QuickCheck
+import Test.QuickCheck.Function
 
 import HW07.JoinList
 import HW07.Sized
@@ -19,28 +21,44 @@ instance (Arbitrary a) => Arbitrary (JoinList Size a) where
               , (3, (+++) <$> arbitrary <*> arbitrary)
               ]
 
-data JLWithIndex = JLWithIndex Int JLSizeBuffer deriving Show
+data JLWithIndex = JLWithIndex Int (JoinList Size Int) deriving Show
 instance Arbitrary JLWithIndex where
   arbitrary = do
     jl <- arbitrary
     n  <- elements [-1 .. jlSize jl + 1]
     return $ JLWithIndex n jl
 
-prop_jToList :: JoinList Size String -> Bool
+prop_functorLaw1 :: (Functor f, Eq (f a))
+                  => f a
+                  -> Bool
+prop_functorLaw1 f = fmap id f == id f
+
+prop_functorLaw2 :: (Functor f, Eq (f c))
+                 => f a
+                 -> Fun b c
+                 -> Fun a b
+                 -> Bool
+prop_functorLaw2 x (apply -> f) (apply -> g) =
+  fmap (f . g) x == (fmap f . fmap g) x
+
+prop_jToList :: JoinList Size Int -> Bool
 prop_jToList jl = jlToList jl == jToList jl
 
 prop_indexJ :: JLWithIndex -> Bool
 prop_indexJ (JLWithIndex i jl) = indexJ i jl == jlToList jl !!? i
 
 prop_takeJ :: JLWithIndex -> Bool
-prop_takeJ (JLWithIndex n jl) = jlToList (takeJ n jl) == take n (jlToList jl)
+prop_takeJ (JLWithIndex n jl) =
+  jlToList (takeJ n jl) == take n (jlToList jl)
 
 prop_dropJ :: JLWithIndex -> Bool
-prop_dropJ (JLWithIndex n jl) = jlToList (dropJ n jl) == drop n (jlToList jl)
+prop_dropJ (JLWithIndex n jl) =
+  jlToList (dropJ n jl) == drop n (jlToList jl)
 
 prop_splitAtJ :: Int -> JoinList Size Int -> Bool
-prop_splitAtJ i jl = let (l1, l2) = splitAtJ i jl
-                     in (jToList l1, jToList l2) == splitAt i (jToList jl)
+prop_splitAtJ i jl =
+  let (l1, l2) = splitAtJ i jl
+  in (jToList l1, jToList l2) == splitAt i (jToList jl)
 -- =============================================================
 
 main :: IO ()
@@ -48,6 +66,8 @@ main = do
   -- =========================
   -- HW07
   -- =========================
+  quickCheck (prop_functorLaw1 :: JoinList Size Int -> Bool)
+  quickCheck (prop_functorLaw2 :: JoinList Size Int -> Fun Int Int -> Fun Int Int -> Bool)
   quickCheck prop_jToList
   quickCheck prop_indexJ
   quickCheck prop_takeJ
